f57a4b4472a0039a3cac6ddced0c0abedcfc2d2b
David Cirella <david.cirella@yale.edu>
2019-11-26 11:46:10 -0500

    add total count to output, add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+#zip_count.sh
+
+This script provides a count of total files held in a directory of zip files with needing to unzip.  
+
+__Input__: Directory of zip files  
+__Output__:  Total count of files held in zip file.  
+
+Call script with a directory path, that holds zip files, as only  argument.  
+
+
+###Requirements
+Common linux commandline tools and:  
+
+- unzip  
+- tee  
+
+Tested on Ubuntu 18.04
+
+
+####Contact:
+David Cirella  
+[github.com/decirella](https://github.com/decirella)
--- zip_count.sh
+++ zip_count.sh
@@ -7,14 +7,22 @@
 # call with argument of directory of zip files
 # count of files within each zip read to stdout and log file
 
+total_files=0
+current_zip_count=0
+
+timestamp=$( date +%Y%m%d-%H%M%S )
+echo "$1" | tee -a zip_count.log
+echo "$timestamp" | tee -a zip_count.log
+
 for file in $(ls -tr $1)
     do
         if /usr/bin/file --mime-type $1/$file | grep -q "zip"; then
-            unzip -l $1/$file  | awk '{count = $2} END{print count}' | tee -a zip_count.log 
-            
+            current_zip_count=$(unzip -l $1/$file  | awk '{count = $2} END{print count}' | tee -a zip_count.log) 
+            total_files=$((total_files + current_zip_count))
         else
             echo "$file is not zipped"  | tee -a zip_count.log
-        fi        
-        
-        timestamp=$( date +%Y%m%d-%H%M%S )
+        fi              
+
     done
+
+echo "Total files: $total_files" | tee -a zip_count.log
